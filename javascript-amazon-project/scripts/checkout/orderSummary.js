0e2a37ebcbc2a786bba1f5945123cd6b138773db
Ash <ashish007tup@gmail.com>
2025-09-07 16:26:31 +0530

    Create automated tests for OrderSummary.js

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -17,7 +17,7 @@ export function renderOrderSummary(){
 
     let cartSummaryHTML = "";
     let cartQty = calculateCartQty();
-    updateCartQuantity();
+    renderCheckoutHeader();
 
 
     cart.forEach((cartItem) => {
@@ -38,7 +38,7 @@ export function renderOrderSummary(){
 
 
         cartSummaryHTML += `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id} js-cart-item-container">
             <div class="delivery-date js-delivery-date-${matchingProduct.id}" data-product-id=${matchingProduct.id}>
                 Delivery Date: ${dateString}
             </div>
@@ -54,7 +54,7 @@ export function renderOrderSummary(){
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
                     Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span>
@@ -63,7 +63,7 @@ export function renderOrderSummary(){
                     </span>
                     <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                     <span class="save-quantity-link link-primary js-save-link" data-product-id=${matchingProduct.id}>Save</span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
+                    <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id=${matchingProduct.id}>
                     Delete
                     </span>
                 </div>
@@ -125,22 +125,17 @@ export function renderOrderSummary(){
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            updateCartQuantity();
+            renderCheckoutHeader();
             //const container = document.querySelector(`.js-cart-item-container-${productId}`)
             //container.remove();
             renderOrderSummary();
-            renderPaymentSummary();
-            renderCheckoutHeader();
+            renderPaymentSummary(); 
 
             //console.log(container);
             console.log(cart);
         });
     });
 
-    function updateCartQuantity() {
-        cartQty = calculateCartQty();
-        document.querySelector('.js-checkout-qty').innerHTML = cartQty;
-    };
 
     document.querySelectorAll('.js-update-link')
         .forEach((link) => {
@@ -176,7 +171,7 @@ export function renderOrderSummary(){
                 );
                 quantityLabel.innerHTML = newQuantity;
 
-                updateCartQuantity();
+                renderCheckoutHeader();
                 renderPaymentSummary();
             });
         });
